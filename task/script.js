128a62e55a5ce3245343b750810a39fe6d4196f8
Alisher Shadiyev <alisher_shax@mail.ru>
2022-11-18 19:50:00 +0500

    Gulp starter use ready

---
+++ task/script.js
@@ -0,0 +1,33 @@
+// Gulp include
+import gulp from "gulp";
+
+// Browser-sync include
+import browserSync from "browser-sync";
+
+// Other plugin
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import webpack from "webpack-stream";
+import babel from "gulp-babel";
+
+// Url include
+import url from "../settings/url.js";
+import option from "../settings/option.js";
+
+// Js task
+export default () => {
+    return gulp.src(url.js.src,{sourcemaps: option.isP})
+    .pipe(plumber({
+        errorHandler: notify.onError(error => ({
+            title: "JS",
+            message: error.message
+        }))
+    }))
+    .pipe(webpack({
+        mode: option.isP ? "production" : "development"
+    }))
+    .pipe(babel())
+    .pipe(gulp.dest(url.js.dest,{sourcemaps: option.isP}))
+    .pipe(browserSync.stream())
+}
+
